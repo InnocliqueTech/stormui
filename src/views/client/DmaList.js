1d73a95004037574f63bea54fe322dee288011c6
Devikumar Indala <Indaladevikumar@gmail.com>
2024-06-24 12:07:38 +0530

    uui corrections

--- src/views/client/DmaList.js
+++ src/views/client/DmaList.js
@@ -7,14 +7,46 @@ import Table from 'react-bootstrap/Table';
 import { Link } from '@mui/material';
 import { MoreVert } from '@mui/icons-material';
 import Paginations from '../../components/Paginatons';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 
 
 export default function DmaList() {
-    let title = '';
-    const location = useLocation();
+    // let title = '';
+    // const location = useLocation();
     return (
+        <>
+         <div className="d-flex justify-content-around row">
+        <div className="col-md-6 col-sm-12 col-12">
+          <nav className='col-md-7'>
+            <ol className="breadcrumb  zone-breadcrum">
+              <li className="breadcrumb-item"><a href="#">Clients</a></li>
+              <li className="breadcrumb-item"><a href="#">Zones</a></li>
+              <li className="breadcrumb-item"><a href="#">DMA’s</a></li>
+              <li className="breadcrumb-item active" aria-current="page">Meters</li>
+            </ol>
+          </nav>
+          <div className="inner-heading">
+            <a >KSCCL-WATER-SUPPLY-OTAA</a>
+          </div>
+        </div>
+        <div className="d-flex justify-content-end col-md-6 col-sm-12 col-12">
+          <div className="row days-filter float-end">
+            <div className="col-md-12">
+              <div className="col-md-1 col-sm-4 col-4" style={{ textAlign: 'end', width: 100, padding: 2 }}>
+                <button className="filter" style={{ background: 'transparent', color: 'rgb(21, 101, 192)', border: '1px solid rgb(21, 101, 192)', paddingLeft: 10, paddingRight: 10, height: 48, width: 103, borderRadius: '8px' }}>
+                  <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="FilterAltOutlinedIcon" style={{ color: 'rgb(21, 101, 192)' }}>
+                    <path d="M7 6h10l-5.01 6.3zm-2.75-.39C6.27 8.2 10 13 10 13v6c0 .55.45 1 1 1h2c.55 0 1-.45 1-1v-6s3.72-4.8 5.74-7.39c.51-.66.04-1.61-.79-1.61H5.04c-.83 0-1.3.95-.79 1.61"></path>
+                  </svg>
+                  Filter
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     
         <div style={{ backgroundColor: '#fff', padding: 10, borderRadius: 10, paddingTop: 30, marginTop: 10, paddingBottom: 100 }}>
+            
             <Row>
                 <Col md={9} sm={7} xs={7}>
                     <span style={{ fontSize: 20, fontWeight: 'bold', color: '#000' }}>Zone List</span> <span style={{ textAlign: 'end' }}><InfoOutlinedIcon style={{ height: 20, width: 20, justifyContent: 'center', color: '#D6D9DC', marginLeft: 5 }} /></span>
@@ -24,12 +56,12 @@ export default function DmaList() {
                 </Col>
             </Row>
             <div className="dashheading">
-                <Link className={title == "Zone List" ? "title" : location.pathname.toLowerCase().includes("list") ? "tab" : "tab active"} to={`/app/${title}`} >{title != "Zone List" ? "Zone List" : title}</Link>
+                {/* <Link className={title == "Zone List" ? "title" : location.pathname.toLowerCase().includes("list") ? "tab" : "tab active"} to={`/app/${title}`} >{title != "Zone List" ? "Zone List" : title}</Link> */}
                 {/* {title != "Dashboard" &&
                         <span>
-                          <Link className={location.pathname.toLowerCase().includes("list") ? "tab active" : "tab"} to={`/app/${title}List`} >{title} List</Link>
+                        <Link className={location.pathname.toLowerCase().includes("list") ? "tab active" : "tab"} to={`/app/${title}List`} >{title} List</Link>
                         </span>
-                      } */}
+                        } */}
             </div>
             <Table responsive style={{ marginTop: 30, borderRadius: 10, border: '1px solid #ccc' }}>
                 <thead style={{ backgroundColor: '#F4F5F5' }}>
@@ -69,5 +101,6 @@ export default function DmaList() {
             </Table>
             <Paginations />
         </div>
+                        </>
     );
 }

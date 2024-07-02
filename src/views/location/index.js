33f9eab8ec08f6defcb4ad76fc199fe1ee138598
Devikumar Indala <Indaladevikumar@gmail.com>
2024-07-02 11:04:24 +0530

    ui changes

--- src/views/location/index.js
+++ src/views/location/index.js
@@ -1,54 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './location.css';  // Import the CSS file
-import image from './image2836.png';
 import HomeEdit from '../../assets/images/HomeEdit.png'
 import { Col } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
-
-const projects = [
-  {
-    title: 'Karimnagar Smart City Corporation',
-    description: '24/7 Water Supply Pilot Project',
-    zones: 6,
-    dmas: 24,
-    meters: 1345,
-    imgSrc: image, // Use the imported image
-  },
-  {
-    title: 'MCK ',
-    description: '24/7 Water Supply Pilot Project',
-    zones: 6,
-    dmas: 24,
-    meters: 1345,
-    imgSrc: image, // Use the imported image
-  },
-  {
-    title: 'Smart City',
-    description: '24/7 Water Supply Pilot Project',
-    zones: 6,
-    dmas: 24,
-    meters: 1345,
-    imgSrc: image, // Use the imported image
-  },
-];
-
+import image from './image2835.png';
+import {ClientsContext} from '../../views/dashboard/context/index'
+import { Link } from 'react-router-dom';
 export default function Location() {
+  const { clients } = useContext(ClientsContext);
   return (
     <>
       <h2 style={{fontSize:32,fontWeight:700, color:'#212121', marginTop:0}} className="">Welcome to Strom Energy</h2>
     <div className="mt-4">
       <div className="row">
-        {projects.map((project, index) => (
+        {clients.map((project, index) => (
           <div className="col-md-4 mb-4" key={index}>
             <div className="card border-0" style={{borderRadius:16}}>
-              <img className="card-img-top" src={project.imgSrc} alt={project.title} />
+              <img className="card-img-top" src={image} alt={project.title} />
               <div className="">
                 <Row className='row p-0 m-0'>
                   <Col className='p-0' md={10} sm={1} xs={1}>
-                    <a style={{color:"black", textDecoration:'none'}} href = "http://49.207.11.223:3308/app/dashboard/default">
-                    <h5 className="card-title m-0 p-0">{project.title}</h5>
-                    </a>
+                  <Link to="/app/dashboard/default">
+  <a style={{ color: "black", textDecoration: 'none' }}>
+    <h5 className="card-title m-0 p-0">{project.clientName}</h5>
+  </a>
+</Link>
                   </Col>
                   
                   <Col md={2} sm={1} xs={1} className='p-0 text-end' >
@@ -60,11 +37,11 @@ export default function Location() {
                 <div className="d-flex justify-content-between">
                   <div className="custom-zones">
                   <small>Zones:</small>
-                  <strong> {project.zones}</strong>
+                  <strong> {project.totalZonesCount}</strong>
                   </div>
                   <div className='custom-zones'>
                     <small>DMAs:</small>
-                    <strong>{project.dmas}</strong> 
+                    <strong>{project.totalDMACount}</strong> 
                   </div>
                   <div className='custom-zones'>
                     <small>Meters:</small>
@@ -77,6 +54,6 @@ export default function Location() {
         ))}
       </div>
     </div>
-        </>
-  );
+  </>
+);
 }

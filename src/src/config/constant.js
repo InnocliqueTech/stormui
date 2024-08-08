8f8442c78e7374fe8d68dd16cb2583029181bca7
KarthikSeelam <119564894+KarthikSeelam@users.noreply.github.com>
2024-08-08 20:55:10 +0530

    Delete src directory

--- src/src/config/constant.js
+++
@@ -1,9 +0,0 @@
-export const BASE_URL = '/app/dashboard/default';
-export const BASE_TITLE = ' | Storm Admin';
-export const BASE_API_URL = 'http://49.207.11.223:3307/api';
-
-export const CONFIG = {
-  layout: 'vertical',
-  collapseMenu: false,
-  layoutType: 'menu-dark'
-};
